28d3f98e161c45152fbf9f621091213f72014d57
sebwelsch <sebjak1@hotmail.com>
2025-03-14 14:35:29 +0100

    fix api url

--- html/js/config.js
+++ html/js/config.js
@@ -1,5 +1,5 @@
 const apiUrl = window.location.hostname === 'localhost'
     ? 'http://localhost:8080' // Local development
-    : 'kinoxp-backend-kea-hqf7bucugnanftfg.northeurope-01.azurewebsites.net'; // Production
+    : 'https://kinoxp-backend-kea-hqf7bucugnanftfg.northeurope-01.azurewebsites.net'; // Production
 
 export default apiUrl;
