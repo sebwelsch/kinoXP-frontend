8d8945f7e277fa5b9d0ade9203dc6ba17626b05e
SkyNetHD <madsmads86@hotmail.com>
2025-03-14 15:27:09 +0100

    fixed login and admin

--- html/js/login.js
+++ html/js/login.js
@@ -1,34 +1,40 @@
 import apiUrl from "./config.js";
 
-document.getElementById("login-button").addEventListener("click", function () {
-    const username = document.getElementById("login-username").value;
-    const password = document.getElementById("login-password").value;
-    const messageElement = document.getElementById("login-message");
+document.addEventListener("DOMContentLoaded", () => {
+    const loginButton = document.getElementById("login-button");
+    const messageElement = document.querySelector(".login-message");
 
-    const data = {username, password};
+    loginButton.addEventListener("click", function () {
+        const username = document.getElementById("login-username").value;
+        const password = document.getElementById("login-password").value;
 
-    fetch(`${apiUrl}/user/login`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json"
-        },
-        body: JSON.stringify(data)
-    })
-        .then(response => {
-            if (!response.ok) {
-                return response.text().then(errorText => {
-                    throw new Error(errorText);
-                });
-            }
-            return response.text();
+        const data = { username, password };
+
+        fetch(`${apiUrl}/user/login`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(data)
         })
-        .then(result => {
-            messageElement.innerText = "Login Successful";
-            console.log("Login Success:", result);
-        })
-        .catch(error => {
-            messageElement.innerText = "Login Failed";
-            console.error("Error:", error);
-        });
+            .then(response => {
+                if (!response.ok) {
+                    return response.text().then(errorText => {
+                        throw new Error(errorText);
+                    });
+                }
+                return response.text();
+            })
+            .then(result => {
+                messageElement.innerText = "Login Successful";
+                console.log("Login Success:", result);
+
+                window.location.href = "admin.html";
+            })
+            .catch(error => {
+                messageElement.innerText = "Login Failed";
+                console.error("Error:", error);
+            });
+    });
 });
